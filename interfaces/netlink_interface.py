deff075835ff662f05087fe38ea9f82263ce1e8b
Ali Ghaffarian <alighaffarian9@gmail.com>
2024-07-25 18:57:45 +0330

    some file rename and logger setup in interfaces/netlink_interface.py

--- interfaces/netlink_interface.py
+++ interfaces/netlink_interface.py
@@ -1,8 +1,23 @@
 import pyroute2
 import logging
-import logging.config
 import os
 import colorlog
+import sys
 
+sys.path.insert(0, os.path.abspath('..'))
+#../logging_utilities/config.py
+import logging_utilities.config as config
+
+#---logger setup---
 FILENAME = os.path.basename(__file__).split('.')[0]
+logger = logging.getLogger(FILENAME)
+logger.setLevel(logging.DEBUG)
+
+stdout_handler = logging.StreamHandler(sys.stdout)
+stdout_handler.setFormatter(config.console_formatter)
+
+logger.addHandler(stdout_handler)
+#---end of logger setup---
+
+
 
