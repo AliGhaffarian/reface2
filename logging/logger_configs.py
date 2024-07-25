3043f17bcb92d3d52899266893c853478dc3b211
Ali Ghaffarian <alighaffarian9@gmail.com>
2024-07-25 18:27:23 +0330

    reface_logger.py rename to logger_configs.py and doc fix

---
+++ logging/logger_configs.py
@@ -0,0 +1,24 @@
+import logging
+import logging.config
+import colorlog
+import sys
+"""
+default configs for the loggers in the rafece2
+"""
+
+# Define the format and log colors
+log_format = '%(asctime)s [%(levelname)s] %(name)s [%(funcName)s]: %(message)s'
+log_colors = {
+        'DEBUG': 'cyan',
+        'INFO': 'green',
+        'WARNING': 'yellow',
+        'ERROR': 'red',
+        'CRITICAL': 'bold_red',
+        }
+
+# Create the ColoredFormatter object
+console_formatter = colorlog.ColoredFormatter(
+        '%(log_color)s' + log_format,
+        log_colors = log_colors
+        )
+
