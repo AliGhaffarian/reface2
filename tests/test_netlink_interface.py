e2317c165f2a50f399850a4bfb409b4f55af8b82
Ali Ghaffarian <alighaffarian9@gmail.com>
2024-07-26 16:49:07 +0330

    fixed generating invalid mac addresses

--- tests/test_netlink_interface.py
+++ tests/test_netlink_interface.py
@@ -7,11 +7,14 @@ import string
 import random
 import time
 import scapy.all
+
+
 sys.path.insert(0, os.path.abspath('..'))
 #../logging_utilities/config.py
 import logging_utilities.config as config
 import interfaces.netlink_interface as netlink_interface
 
+import mac_templates
 #---logger setup---
 FILENAME = os.path.basename(__file__).split('.')[0]
 logger = logging.getLogger(FILENAME)
@@ -40,7 +43,7 @@ if len(config["allowed_interfaces_to_manipulate"]) == 0:
 else:
     interfaces_to_manipulate = config["allowed_interfaces_to_manipulate"]
   
-
+logger.info(f"interfaces to test on: {interfaces_to_manipulate}")
 
 def list_interface_names():
     result = []
@@ -50,7 +53,7 @@ def list_interface_names():
 
 def test_interface_exists():
     interface_names = list_interface_names()
-    logger.debug(f"testing with interface list {interface_names=}")
+    logger.debug(f"[*] testing with interface list {interface_names=}")
     
     #making invalid interface names
     invalid_interface_names = []
@@ -68,18 +71,18 @@ def test_interface_exists():
 
     for name in invalid_interface_names:
         assert netlink_interface.interface_exists(name) == False
-        logger.debug("[+] passed on {name=}, expected to be False")
+        logger.debug(f"[+] passed on {name=}, expected to be False")
 
     logger.info("[+] passed")
 
 def dump_info_for(interface_name):
     interface_info, = IPR.link('dump', ifname=interface_name)
     if interface_info is None:
-        logger.debug(f"dumping for {interface_name=} failed... polling")
+        logger.debug(f"[*] dumping for {interface_name=} failed... polling")
         interface_info, = IPR.poll(IPR.link, 'dump', timeout=config['poll_timeout'], ifname=interface_name)
         
         if interface_info is None:
-            logger.error(f"can't dump info for {interface_name=}")
+            logger.error(f"[!] can't dump info for {interface_name=}")
     return interface_info
 
 def test_update_interface_state():
@@ -101,7 +104,7 @@ def test_update_interface_state():
                 assert dump_info_for(interface)['state'] == 'up'
 
             else:
-                logger.critical(f"un expected event!!! {interface_info['state']=}")
+                logger.critical(f"[!] un expected event!!! {interface_info['state']=}")
 
     
       
@@ -110,23 +113,33 @@ def test_update_interface_state():
     
     logger.info(f"[+] passed")
 
+
 def test_change_interface_mac():
     for interface_name in interfaces_to_manipulate:
         netlink_interface.update_interface_state(interface_name, 'down')
         time.sleep(config['interface_change_state_sleep'])
         if(dump_info_for(interface_name)['state'] == 'up'):
-            logger.warning(f"{interface_name=} didn't go down for mac change")
+            logger.warning(f"[!] {interface_name=} didn't go down for mac change")
 
-        mac = str(scapy.all.RandMAC())
+        mac_template = mac_templates.templates[random.choice(range(len(mac_templates.templates) - 1))][0]
+        mac = str(scapy.all.RandMAC(mac_template))
+
         logger.debug(f"setting {mac=} for interface{interface_name=}")
-        netlink_interface.set_interface_mac(interface_name, mac)
+        
+        try:
+            netlink_interface.set_interface_mac(interface_name, mac)
+        except Exception as e:
+            logger.warning(f"[!] {e}")
+            logger.warning(f"{mac=} {interface_name=}")
+
         netlink_interface.update_interface_state(interface_name, 'up')
         time.sleep(config['interface_change_state_sleep'])
         
         if(dump_info_for(interface_name)['state'] == 'down'):
-            logger.warning(f"{interface_name=} didn't startup")
-        
+            logger.warning(f"[!] {interface_name=} didn't startup")
+
         assert dump_info_for(interface_name).get_attr('IFLA_ADDRESS') == mac
+        logger.debug(f"[+] passed {interface_name=} == {mac=}") 
         
     logger.info("[+] passed")
 
