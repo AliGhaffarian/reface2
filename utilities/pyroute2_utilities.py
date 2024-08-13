2184b51ea077f48585650c89875b694ede01a914
Ali Ghaffarian <alighaffarian9@gmail.com>
2024-08-13 19:43:46 +0330

    exception handling, impl set_host_data

--- utilities/pyroute2_utilities.py
+++ utilities/pyroute2_utilities.py
@@ -5,7 +5,6 @@ import sys
 sys.path.insert(0, os.path.abspath("../../"))
 import errno
 
-import reface2.utilities.constants.linux_errors as linux_errors
 import reface2.utilities.logging_utilities.config as config
 
 
@@ -40,9 +39,7 @@ def interface_list():
 def change_mac(interface_name : str, new_mac : str)->bool:
     
     if interface_name not in interface_list():
-        logger.warning(f"{interface_name} doesn't exist doing nothing")
-        logger.debug(f"interface_list={interface_list()}")
-        return linux_errors.ENODEV
+        raise Exception(errno.ENODEV, os.strerror(errno.ENODEV))
 
     mylink = ipr.poll(ipr.link, "dump", ifname=interface_name, timeout=conf[TIMEOUT])[0]
     logger.debug(f"dumped info for interface {interface_name=}")
@@ -68,16 +65,16 @@ def change_mac(interface_name : str, new_mac : str)->bool:
         result = ipr.poll(ipr.link, "set", ifname=interface_name, address=new_mac, timeout =conf[TIMEOUT])[0]
         logger.debug(f"result of set mac {result}")
     except pyroute2.NetlinkError as e:
-        if e.code != linux_errors.EBUSY:
-            logger.critical(e)
-            return e.code
+        if e.code != errno.EBUSY:
+            raise e
 
         logger.info(f"{interface_name} is busy shutting down the interface")
 
         temp_result = ipr.poll(ipr.link, "set", ifname=interface_name, state="down", timeout=conf[TIMEOUT])[0]
+
         if temp_result['error']:
-            logger.error(f"failed to shutdown {interface_name}")
-            return  temp_result['error']
+            raise Exception(temp_result['error'], os.strerror(temp_result['error']))
+
         logger.debug(f"result of shutting down the interface: {temp_result}")
 
         result = ipr.poll(ipr.link, "set", ifname=interface_name, address=new_mac, timeout =conf[TIMEOUT])[0]
@@ -86,8 +83,28 @@ def change_mac(interface_name : str, new_mac : str)->bool:
         temp_result = ipr.poll(ipr.link, "set", ifname=interface_name, state="up", timeout=conf[TIMEOUT])[0]
         logger.debug(f"result of starting up the interface: {temp_result}")
     except Exception as e:
-        logger.critical(f"{e}")
-        return e.code
+        raise e
     
     #return error code, usually 0
     return result['error']
+
+
+def set_host_data(ifname, ip, mac, ip_netmask=32, ttl=None):
+    """
+    adds ip address and sets mac address on the interface
+    """
+    try:
+        ipr.addr("add", index=ipr.link_lookup(ifname=ifname)[0], address=ip, prefixlen=ip_netmask)
+    except Exception as e:
+        if e.code != EEXIST:
+            raise e
+    change_mac(ifname, mac)
+
+def switch_to_ip(ifname, ip, netmask):
+    #check all routes for ifname
+    #1_delete routes for networks with <= netmask
+    #2_add a route to the target network with prefered ip of {ip}
+    #3_if the network was default gateway set a default gateway too
+    #what about other interfaces with the same network dest?
+        #if we got a prefered src to that network we should be good if the other interfaces are shutdown
+    raise NotImplementedError
